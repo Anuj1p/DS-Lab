d0077fca8d4f0f4173e590060b33a0bfcd0aff1e
Anuj <54796767+Anuj1p@users.noreply.github.com>
2020-04-17 23:35:05 +0530

    Add files via upload

---
+++ J(b)  Selection Sort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+//swapping two words
+void swap(int *x,int *y){
+    int temp;
+    temp = *x;
+    *x=*y;
+    *y=temp;
+}
+
+void selectionSort(int arr[],int n){
+    int i,j,min;
+    for (i = 0; i < n-1; i++)
+    {
+        min=i;
+        for(j=i+1;j<n;j++){         //marker
+            if(arr[j]<arr[min]){
+                min = j;
+            }
+        }
+        swap(&arr[i],&arr[min]);
+    }
+
+}
+int main(){
+    int arr[]={2,5,28,70,4,100,50};
+    selectionSort(arr,7);
+    int i;
+    for ( i = 0; i < 7; i++)
+    {
+        printf("%d \t",arr[i]);
+    }
+
+    return 0;
+}
